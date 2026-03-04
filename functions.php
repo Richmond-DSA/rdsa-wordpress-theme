85345c62d01319add11ccc0a4991c54657467c18
ajmarvingomez <ajmarvingomez@gmail.com>
2026-03-03 21:17:48 -0500

    feature: Remove extraneous inc calls

--- functions.php
+++ functions.php
@@ -116,40 +116,6 @@ endif;
 add_action('after_setup_theme', 'dsarichmond_setup');
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function dsarichmond_content_width()
-{
-	$GLOBALS['content_width'] = apply_filters('dsarichmond_content_width', 640);
-}
-add_action('after_setup_theme', 'dsarichmond_content_width', 0);
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function dsarichmond_widgets_init()
-{
-	register_sidebar(
-		array(
-			'name' => esc_html__('Sidebar', 'dsarichmond'),
-			'id' => 'sidebar-1',
-			'description' => esc_html__('Add widgets here.', 'dsarichmond'),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget' => '</section>',
-			'before_title' => '<h2 class="widget-title">',
-			'after_title' => '</h2>',
-		)
-	);
-}
-add_action('widgets_init', 'dsarichmond_widgets_init');
-
-/**
  * Enqueue scripts and styles.
  */
 function dsarichmond_scripts()
@@ -166,33 +132,6 @@ function dsarichmond_scripts()
 	}
 }
 add_action('wp_enqueue_scripts', 'dsarichmond_scripts');
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if (defined('JETPACK__VERSION')) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
 
 /******* NEW CODE: ************/
 function rdsa_custom_blocks()
