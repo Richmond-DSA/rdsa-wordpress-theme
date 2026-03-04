f989a1682c2439bdb8e7e9ed0e5d0102c669fb3d
ajmarvingomez <ajmarvingomez@gmail.com>
2026-03-03 21:07:18 -0500

    fix: Enable mailchimp block in theme

--- build/custom-blocks/mailchimp/index.asset.php
+++ build/custom-blocks/mailchimp/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks'), 'version' => '686ba88e133326f63a32');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks'), 'version' => '687a56432a5c368f5fc7');
